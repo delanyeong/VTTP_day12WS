84935917bec4faf1794e7ec66783ba64d7a911f1
delanyeong <delan.april@gmail.com>
2022-07-19 11:50:34 +0800

    CartService added

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartService.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartService {
+    
+    public List<Item> getShoppingItems() {
+    
+        List<Item> listItems = new ArrayList<>();
+
+        Item itm = new Item();
+        itm.setItemName("Prada bag");
+        itm.setQuantity(5);
+        listItems.add(itm);
+
+        itm = new Item();
+        itm.setItemName("Macbook Pro");
+        itm.setQuantity(10);
+        listItems.add(itm);
+
+        itm = new Item();
+        itm.setItemName("LV Tote Bag");
+        itm.setQuantity(8);
+        listItems.add(itm);
+
+        return listItems;
+    }
+}
---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/Item.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.iss.day12wkshp;
+
+public class Item {
+    private String itemName;
+    private Integer quantity;
+
+
+    public Item() {
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    
+}
